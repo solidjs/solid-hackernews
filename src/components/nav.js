b25a548be05eefdb45b0b93046c347ea1c201090
Caleb Taylor <aquaductape@gmail.com>
2020-10-31 14:52:11 -0700

    replaces b tag with strong tag for modern and semantic purpose

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,19 +5,19 @@ export default function Nav() {
     <header class="header">
       <nav class="inner">
         <Link href="">
-          <b>HN</b>
+          <strong>HN</strong>
         </Link>
         <Link href="new">
-          <b>New</b>
+          <strong>New</strong>
         </Link>
         <Link href="show">
-          <b>Show</b>
+          <strong>Show</strong>
         </Link>
         <Link href="ask">
-          <b>Ask</b>
+          <strong>Ask</strong>
         </Link>
         <Link href="job">
-          <b>Jobs</b>
+          <strong>Jobs</strong>
         </Link>
         <a class="github" href="http://github.com/ryansolid/solid" target="_blank" rel="noreferrer">
           Built with Solid
