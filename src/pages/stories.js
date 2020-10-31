6287ed436b9d8d24fe5acb2ac89bbd8f3a4dc456
Caleb Taylor <aquaductape@gmail.com>
2020-10-31 15:07:08 -0700

    changed fallback to display span. Anchor tags should have href property, and since there's no disable property it's better to render a generic element instead

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -5,21 +5,47 @@ export default function Stories(props) {
   return (
     <div class="news-view">
       <div class="news-list-nav">
-        <Show when={props.page > 1} fallback={<a class="disabled">&lt; prev</a>}>
-          <Link href={`${props.type}?page=${props.page - 1}`}>{"<"} prev</Link>
+        <Show
+          when={props.page > 1}
+          fallback={
+            <span class="page-link disabled" aria-hidden="true">
+              &lt; prev
+            </span>
+          }
+        >
+          <Link
+            class="page-link"
+            href={`${props.type}?page=${props.page - 1}`}
+            aria-label="Previous Page"
+          >
+            {"<"} prev
+          </Link>
         </Show>
         <span>page {props.page}</span>
-        <Show when={props.stories?.length === 30} fallback={<a class="disabled">more &gt;</a>}>
-          <Link href={`${props.type}?page=${props.page + 1}`}>more {">"}</Link>
+        <Show
+          when={props.stories?.length === 30}
+          fallback={
+            <span class="page-link" aria-hidden="true">
+              more &gt;
+            </span>
+          }
+        >
+          <Link
+            class="page-link"
+            href={`${props.type}?page=${props.page + 1}`}
+            aria-label="Next Page"
+          >
+            more {">"}
+          </Link>
         </Show>
       </div>
-      <div class="news-list">
+      <main class="news-list">
         <Show when={props.stories}>
           <ul>
             <For each={props.stories}>{(story) => <Story story={story} />}</For>
           </ul>
         </Show>
-      </div>
+      </main>
     </div>
   );
 }
