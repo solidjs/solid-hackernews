54bb801b603acf6d600e590c6ee5339e8187e884
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2021-01-24 20:56:44 +0100

    :bug: Fix service worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
-self.addEventListener("fetch",e=>{(e.request.url.includes("localhost")||e.request.url.includes("ryansolid"))&&e.respondWith(caches.open("solid-hn").then(t=>t.match(e.request).then(n=>n||fetch(e.request).then(n=>(t.put(e.request,n.clone()),n)))))}),self.addEventListener("activate",e=>e.waitUntil(caches.delete("solid-hn")))
+self.addEventListener("fetch", (e) => {
+  (e.request.url.includes("localhost") || e.request.url.includes("netlify")) &&
+    e.respondWith(
+      caches
+        .open("solid-hn")
+        .then((t) =>
+          t
+            .match(e.request)
+            .then((n) => n || fetch(e.request).then((n) => (t.put(e.request, n.clone()), n)))
+        )
+    );
+});
+
+self.addEventListener("activate", (e) => e.waitUntil(caches.delete("solid-hn")));
