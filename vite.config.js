07f4aeeb437dabba76bc90133849569f315fa6b7
Alexandre <amoutonbrady@gmail.com>
2021-03-01 22:35:36 +0100

    :sparkles: Adding basic route manifest

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import manifest from "rollup-route-manifest";
+import path from "path";
 
 export default defineConfig({
-  plugins: [solid({ ssr: true })],
+  plugins: [
+    solid({ ssr: true }),
+    manifest({
+      inline: false,
+      merge: false,
+      publicPath: "/assets/",
+      routes: (file) => {
+        file = file.replace(path.join(__dirname, "src"), "").replace(/\.[tj]sx?$/, "");
+        if (!file.includes("/pages/")) return "*"; // commons
+        let name = "/" + file.replace("/pages/", "").toLowerCase();
+        return name === "/home" ? "/" : name;
+      }
+    })
+  ],
   build: {
     polyfillDynamicImport: false,
     target: "esnext"
