751451784bc9869b0ba5507309e68f9a0700aafb
Ryan Carniato <ryansolid@gmail.com>
2021-11-05 02:11:20 -0700

    create netlify version

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
-import vercel from "solid-start-vercel-edge";
+import netlify from "solid-start-netlify";
 
 export default defineConfig({
-  plugins: [solid({ adapter: vercel() })],
+  plugins: [solid({ adapter: netlify() })],
   build: {
     rollupOptions: {
       output: {
